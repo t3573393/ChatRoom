15b9057f309d01ca74405e299167bbd4af53ddf1
t3573393 <283616181@qq.com>
2026-05-10 00:14:21 +0000

    feat: add roomManagementService.js frontend service

---
+++ public/app/services/roomManagementService.js
@@ -0,0 +1,63 @@
+'use strict';
+
+app.factory('roomManagementService', ['$rootScope', 'socket', function($rootScope, socket) {
+    var service = {};
+    
+    service.kickUser = function(targetUsername, callback) {
+        socket.emit('kick-user', {
+            targetUsername: targetUsername
+        }, function(response) {
+            if (callback) {
+                callback(response);
+            }
+        });
+    };
+    
+    service.muteUser = function(targetUsername, callback) {
+        socket.emit('mute-user', {
+            targetUsername: targetUsername
+        }, function(response) {
+            if (callback) {
+                callback(response);
+            }
+        });
+    };
+    
+    service.unmuteUser = function(targetUsername, callback) {
+        socket.emit('unmute-user', {
+            targetUsername: targetUsername
+        }, function(response) {
+            if (callback) {
+                callback(response);
+            }
+        });
+    };
+    
+    service.isRoomCreator = function() {
+        return $rootScope.isRoomCreator || false;
+    };
+    
+    service.isMuted = function(username, mutedUsers) {
+        return mutedUsers && mutedUsers.includes(username);
+    };
+    
+    service.registerEventListeners = function($scope) {
+        socket.on('you-have-been-kicked', function(data) {
+            $scope.$emit('you-have-been-kicked', data);
+        });
+        
+        socket.on('user-kicked', function(data) {
+            $scope.$emit('user-kicked', data);
+        });
+        
+        socket.on('user-muted', function(data) {
+            $scope.$emit('user-muted', data);
+        });
+        
+        socket.on('user-unmuted', function(data) {
+            $scope.$emit('user-unmuted', data);
+        });
+    };
+    
+    return service;
+}]);
